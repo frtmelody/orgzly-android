c2f04cefc8872ad3fec02364202a4b7bfcc71769
Rafael <rafaelleru95103@gmail.com>
2017-03-25 22:43:46 +0100

    webDAVRepo implemented.

--- app/src/main/java/com/orgzly/android/repos/WebDAVRepo.java
+++ app/src/main/java/com/orgzly/android/repos/WebDAVRepo.java
@@ -3,6 +3,12 @@ package com.orgzly.android.repos;
 import android.content.Context;
 import android.net.Uri;
 
+import com.orgzly.android.util.UriUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by rafa on 25/03/17.
  */
@@ -18,7 +24,45 @@ public class WebDAVRepo implements Repo {
     }
 
     @Override
-    Uri getUri() {
-        return this.uri;
+    public boolean requiresConnection() {
+        return true;
     }
+
+    @Override
+    public Uri getUri() {
+        return this.repoUri;
+    }
+
+    @Override
+    public List<VersionedRook> getBooks() throws IOException {
+        return client.getBooks(repoUri);
+    }
+
+    @Override
+    public VersionedRook retrieveBook(Uri uri, File file) throws IOException {
+        return client.download(repoUri, uri, file);
+    }
+
+    @Override
+    public VersionedRook storeBook(File file, String fileName) throws IOException {
+        return client.upload(file, repoUri, fileName);
+    }
+
+    @Override
+    public VersionedRook renameBook(Uri fromUri, String name) throws IOException {
+        Uri toUri = UriUtils.getUriForNewName(fromUri, name);
+        return client.move(repoUri, fromUri, toUri);
+    }
+
+    @Override
+    public void delete(Uri uri) throws IOException {
+        client.delete(uri.getPath());
+    }
+
+    @Override
+    public String toString() {
+        return repoUri.toString();
+    }
+
+
 }
